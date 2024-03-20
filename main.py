c65e02b5a853e7060c9df93f36aa67b41db1d2dd
Przemek <przemyslaw.makowka@o2.pl>
2024-03-20 14:07:28 +0100

    Change printing board in funtion print_board

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def print_board(board_game: list):
     row = 1
-    print('  1 2 3')
+    bord_len = len(board_game)
+    print(' ', end='')
+    for i in range(bord_len):
+        print(f' {i+1}',end='')
+    print()
     for i in board_game:
         print(row, end=' ')
         for j in i:
