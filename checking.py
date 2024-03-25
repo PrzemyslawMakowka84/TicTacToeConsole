51935c3391c3c674c9d8a64bae014293ff4ae85d
Przemek <przemyslaw.makowka@o2.pl>
2024-03-25 16:56:15 +0100

    Refactoring code - added new function to coloring winning sybols

--- checking.py
+++ checking.py
@@ -1,28 +1,20 @@
 from colorama import Fore, Style
 
+
 def check_if_win(board_game: list):
     board_len = len(board_game)
-    winning_symbol = ''
     for i in range(0, board_len):
         if all(cell == 'X' for cell in [board_game[i][cell] for cell in range(board_len)]):
-            winning_symbol = f'{Fore.LIGHTRED_EX}X{Style.RESET_ALL}'
-            for cell in range(board_len):
-                board_game[i][cell] = winning_symbol
+            coloring_winning_symbols(board_game, i, None, 'X')
             return True
         elif all(cell == 'O' for cell in [board_game[i][cell] for cell in range(board_len)]):
-            winning_symbol = f'{Fore.LIGHTRED_EX}O{Style.RESET_ALL}'
-            for cell in range(board_len):
-                board_game[i][cell] = winning_symbol
+            coloring_winning_symbols(board_game, i, None, 'O')
             return True
         if all(cell == 'X' for cell in [board_game[cell][i] for cell in range(board_len)]):
-            winning_symbol = f'{Fore.LIGHTRED_EX}X{Style.RESET_ALL}'
-            for cell in range(board_len):
-                board_game[cell][i] = winning_symbol
+            coloring_winning_symbols(board_game, None, i, 'X')
             return True
         elif all(cell == 'O' for cell in [board_game[cell][i] for cell in range(board_len)]):
-            winning_symbol = f'{Fore.LIGHTRED_EX}O{Style.RESET_ALL}'
-            for cell in range(board_len):
-                board_game[cell][i] = winning_symbol
+            coloring_winning_symbols(board_game, None, i, 'O')
             return True
     temp_list = []
     n = 0
@@ -32,14 +24,10 @@ def check_if_win(board_game: list):
             n += 1
 
     if all(cell == 'X' for cell in temp_list) and n == board_len:
-        winning_symbol = f'{Fore.LIGHTRED_EX}X{Style.RESET_ALL}'
-        for i in range(board_len):
-            board_game[i][i] = winning_symbol
+        coloring_winning_symbols(board_game, None, None, 'X', True)
         return True
     elif all(cell == 'O' for cell in temp_list) and n == board_len:
-        winning_symbol = f'{Fore.LIGHTRED_EX}O{Style.RESET_ALL}'
-        for i in range(board_len):
-            board_game[i][i] = winning_symbol
+        coloring_winning_symbols(board_game, None, None, 'O', True)
         return True
 
     n = 0
@@ -50,14 +38,10 @@ def check_if_win(board_game: list):
             n += 1
 
     if all(cell == 'X' for cell in temp_list) and n == board_len:
-        winning_symbol = f'{Fore.LIGHTRED_EX}X{Style.RESET_ALL}'
-        for i in range(board_len):
-            board_game[i][(board_len - 1) - i] = winning_symbol
+        coloring_winning_symbols(board_game, None, None, 'X', False)
         return True
     elif all(cell == 'O' for cell in temp_list) and n == board_len:
-        winning_symbol = f'{Fore.LIGHTRED_EX}O{Style.RESET_ALL}'
-        for i in range(board_len):
-            board_game[i][(board_len - 1) - i] = winning_symbol
+        coloring_winning_symbols(board_game, None, None, 'O', False)
         return True
     return False
 
@@ -67,9 +51,7 @@ def check_if_draw(board_game: list):
     board_len = len(board_game)
     for i in board_game:
         if_win = check_if_win(board_game)
-        if all(cell == 'X'
-               or cell == 'O'
-               for cell in i) and not if_win:
+        if all(cell == 'X' or cell == 'O' for cell in i) and not if_win:
             n += 1
     if n == board_len:
         return True
@@ -94,3 +76,20 @@ def check_variables_input(symbol: str, board_len: int):
         except ValueError:
             print('You are trying input impermissible character(s)! Try again!')
     return x, y
+
+
+def coloring_winning_symbols(board_game, row, col, symbol, is_main_diagonal=True):
+    winning_symbol = f'{Fore.LIGHTRED_EX}{symbol}{Style.RESET_ALL}'
+    board_len = len(board_game)
+    if row is not None:
+        for cell in range(board_len):
+            board_game[row][cell] = winning_symbol
+    elif col is not None:
+        for cell in range(board_len):
+            board_game[cell][col] = winning_symbol
+    elif row is None and col is None and is_main_diagonal:
+        for i in range(board_len):
+            board_game[i][i] = winning_symbol
+    elif row is None and col is None and not is_main_diagonal:
+        for i in range(board_len):
+            board_game[i][len(board_game) - 1 - i] = winning_symbol
